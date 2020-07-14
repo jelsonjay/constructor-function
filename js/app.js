27483230393a8e8a35c426af542e55d1ceebfb04
jelson <jelson20@outlook.com>
2020-07-14 21:22:02 +0100

    Commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 'use strict';
-function Car(name, mark) {
-	this.name = name;
+function Car(color, mark, year) {
+	this.color = color;
 	this.mark = mark;
+	this.year = year;
 }
 
-let audi = new Car({ name: 'Audi', mark: 'A7' });
+let audi = new Car('Blue', 'Audi A7', 2014);
+let mercedes = new Car('Whait', 'Mercedes - Benz', 2018);
+let lexuus = new Car('Black', 'Lexus', 2019);
+let bmw = new Car('Orange', 'BMW 3 Series‎', 2016);
+let porche = new Car('Red', 'Porsche 911', 2011);
 
 console.log(audi);
