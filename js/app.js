9fe2d0e21d0902b33f5d2025443b8f193452d3c2
jelson <jelson20@outlook.com>
2020-07-14 21:36:55 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 'use strict';
+// Constructor function for Car objects
 function Car(color, mark, year) {
 	this.color = color;
 	this.mark = mark;
 	this.year = year;
 }
 
+// Create a Person object
 let audi = new Car('Blue', 'Audi A7', 2014);
 let mercedes = new Car('Whait', 'Mercedes - Benz', 2018);
 let lexuus = new Car('Black', 'Lexus', 2019);
@@ -14,5 +16,6 @@ let lambor = new Car('Gray', 'Lamborghini', 2013);
 let jaguar = new Car('Yellow', 'Jaguar XE', 2018);
 let landerRover = new Car('White', 'Rover Range Rover Sport', 2020);
 
+// Display result
 console.log(audi);
-document.querySelector('#app').innerHTML = audi;
+document.querySelector('#app').innerHTML = bmw.mark;
