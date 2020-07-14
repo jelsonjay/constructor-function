281b4cb0c045e092cb4a2d9c7854d49756ad719f
jelson <jelson20@outlook.com>
2020-07-14 21:30:23 +0100

    Commit

--- js/app.js
+++ js/app.js
@@ -10,5 +10,9 @@ let mercedes = new Car('Whait', 'Mercedes - Benz', 2018);
 let lexuus = new Car('Black', 'Lexus', 2019);
 let bmw = new Car('Orange', 'BMW 3 Series‎', 2016);
 let porche = new Car('Red', 'Porsche 911', 2011);
+let lambor = new Car('Gray', 'Lamborghini', 2013);
+let jaguar = new Car('Yellow', 'Jaguar XE', 2018);
+let landerRover = new Car('White', 'Rover Range Rover Sport', 2020);
 
 console.log(audi);
+document.querySelector('#app').innerHTML = audi;
