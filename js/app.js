9c777e2072bf52a2435c95ab960ff1184757f512
jelson <jelson20@outlook.com>
2020-07-14 22:05:30 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -11,15 +11,15 @@ function Car(color, mark, year, plate) {
 }
 
 // Create a Person object
-let audi = new Car('Blue', 'Audi A7', 2014);
-let mercedes = new Car('Whait', 'Mercedes - Benz', 2018);
-let lexuus = new Car('Black', 'Lexus', 2019);
-let bmw = new Car('Orange', 'BMW 3 Series‎', 2016);
-let porche = new Car('Red', 'Porsche 911', 2011);
-let lambor = new Car('Gray', 'Lamborghini', 2013);
+let audi = new Car('Blue', 'Audi A7', 2014, 'AQW85421');
+let mercedes = new Car('Whait', 'Mercedes - Benz', 2018, 'HGV58741');
+let lexuus = new Car('Black', 'Lexus', 2019, 'QKU8541');
+let bmw = new Car('Orange', 'BMW 3 Series‎', 2016, 'WSDF258');
+let porche = new Car('Red', 'Porsche 911', 2011, 'TRD2547');
+let lambor = new Car('Gray', 'Lamborghini', 2013, 'KYT7854');
 let jaguar = new Car('Yellow', 'Jaguar XE', 2018, 'HTJ8562');
-let landerRover = new Car('White', 'Rover Range Rover Sport', 2020);
+let landerRover = new Car('White', 'Rover Range Rover Sport', 2020, 'OUY87412');
 
 // Display result
 console.log(jaguar);
-document.querySelector('#app').innerHTML = bmw.mark;
+document.querySelector('#app').innerHTML = 'My Favourite car is ' + jaguar.mark;
